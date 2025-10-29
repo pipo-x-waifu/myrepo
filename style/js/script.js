f6db0b977d0ad8a14a5a7e8ea8a3dd205eb7b4a1
pipo-x-waifu <pipokenthang4@gmail.com>
2025-10-29 14:19:17 +0700

    Initial commit

--- style/js/script.js
+++ style/js/script.js
@@ -4,41 +4,48 @@ var profile = document.getElementsByClassName("profile");
 var user = "";
 var n = 0;
 
-//jika fungsi dipanggip maka variabel user akan ter isi
+// untuk mengisi variabel user
 function save_username() {
-    user = prompt("Masukan nama anda!");
+    user = prompt("Masukan nama anda!")
 }
 
-//jika anda menekan oke maka akan otomatis keluar dari peramban
-function keluar() {
-    var con = confirm("\t\tNama diperlukan!\n\nApakah kamu ingin keluar?.\nJika jika iya tekan oke");
-    var result = (con == true) ? window.close() : home();
+function confirm_keluar() {
+    if(confirm("Ulangi tekan oke")){
+        home();
+    } else {
+        window.close();
+    }
 }
 
-// Memanggil fungsi save_username() untuk mengisi variabel user
-// mengecek apakah variabel user sudah ter isi atau belum jika sudah akan mengarah ke halaman
-// Dan jika belum terisi maka function keluar dipanggil, function tersebut berisi confirmasi apakah user ingin keluar
-// atau lanjut jika lanjut maka username harus diisi
+// Memanggil Memanggil fungsi home()
+// Mengecek apakah variabel user sudah ter isi atau belum jika sudah akan mengarah ke halaman index
+// Jika belum ada isi maka akan keluar dari halaman
+
 function home() {
     save_username()
     if ( user ) {
         alert("Welcome to my page " + user);
+        // menulis ke element p dengan id welcomeUser
+        document.getElementById("welcomeUser").innerText ="Selamat datang " + user + " semoga harimu baik baik saja";
+        // menambahkan dan menghapus class pada element
+        navbar_button[0].addEventListener('click', () => {
+            n+=1;
+            if ( n == 1 ){
+                navbar_brand[0].classList.remove("act")
+                navbar_brand[0].classList.toggle("none");
+                profile[0].style.display = "block";
+            } else if ( n == 2 ) {
+                navbar_brand[0].classList.remove("none");
+                navbar_brand[0].classList.toggle("act");
+                profile[0].style.display = "none";
+                n = 0;
+            }
+        });
     } else {
-        keluar()
+        setTimeout(() => {
+            confirm_keluar()
+        }, 500);
+        alert("Nama diperlukan!");
     }
+}
 home()
-
-// menambahkan dan menghapus class pada element
-navbar_button[0].addEventListener('click', () => {
-    n+=1;
-    if ( n == 1 ){
-        navbar_brand[0].classList.remove("act")
-        navbar_brand[0].classList.toggle("none");
-        profile[0].style.display = "block";
-    } else if ( n == 2 ) {
-        navbar_brand[0].classList.remove("none");
-        navbar_brand[0].classList.toggle("act");
-        profile[0].style.display = "none";
-        n = 0;
-    }
-});
