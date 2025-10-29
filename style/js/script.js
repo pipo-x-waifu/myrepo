edc08d42d76fd9cb58fb8d8f3a3ee4f2eade15d5
pipo-x-waifu <pipokenthang4@gmail.com>
2025-10-29 13:08:16 +0700

    menambahkan sedikit perubaha pada kode script.js

--- style/js/script.js
+++ style/js/script.js
@@ -1,8 +1,31 @@
 var navbar_button = document.getElementsByClassName("navbar-toggler-icon");
 var navbar_brand = document.getElementsByClassName("navbar-brand");
 var profile = document.getElementsByClassName("profile");
+var user = "";
 var n = 0;
 
+//jika fungsi dipanggip maka variabel user akan ter isi
+function save_username() {
+    user = prompt("Masukan nama anda!");
+}
+
+//jika anda menekan oke maka akan otomatis keluar dari peramban
+function keluar() {
+    var con = confirm("Apakah kamu ingin keluar?.\nJika jika iya tekan oke");
+    var result = (con == true) ? window.close() : save_username();
+    return result
+}
+
+// mengecek apakah variabel user sudah ter isi atau belum jika sudah akan mengarah ke halaman
+// Dan jika belum terisi maka function keluar dipanggil, function tersebut berisi confirmasi apakah user ingin keluar
+// atau lanjut jika lanjut maka username harus diisi
+if ( user.length < 0 ) {
+    keluar()
+} else {
+    alert("Welcome to my page " + user);
+}
+
+// menambahkan dan menghapus class pada element
 navbar_button[0].addEventListener('click', () => {
     n+=1;
     if ( n == 1 ){
