44dcefab501eec277fc02b5b21ee7d4ee9f4b80b
pipo-x-waifu <pipokenthang4@gmail.com>
2026-01-03 22:34:19 +0700

    Refactor home function and add async fetch
    Refactor home function to use async fetch for welcome message and handle user input more effectively.

--- style/js/script.js
+++ style/js/script.js
@@ -1,51 +1,39 @@
-var navbar_button = document.getElementsByClassName("navbar-toggler-icon");
-var navbar_brand = document.getElementsByClassName("navbar-brand");
-var profile = document.getElementsByClassName("profile");
-var user = "";
-var n = 0;
-
-// untuk mengisi variabel user
-function save_username() {
-    user = prompt("Masukan nama anda!")
-}
-
-function confirm_keluar() {
-    if(confirm("Ulangi tekan oke")){
-        home();
-    } else {
-        window.close();
-    }
-}
-
-// Memanggil Memanggil fungsi home()
-// Mengecek apakah variabel user sudah ter isi atau belum jika sudah akan mengarah ke halaman index
-// Jika belum ada isi maka akan keluar dari halaman
-
-function home() {
-    save_username()
-    if ( user ) {
-        alert("Welcome to my page " + user);
-        // menulis ke element p dengan id welcomeUser
-        document.getElementById("welcomeUser").innerText ="Selamat datang " + user + " semoga harimu baik baik saja";
-        // menambahkan dan menghapus class pada element
-        navbar_button[0].addEventListener('click', () => {
-            n+=1;
-            if ( n == 1 ){
-                navbar_brand[0].classList.remove("act")
-                navbar_brand[0].classList.toggle("none");
-                profile[0].style.display = "block";
-            } else if ( n == 2 ) {
-                navbar_brand[0].classList.remove("none");
-                navbar_brand[0].classList.toggle("act");
-                profile[0].style.display = "none";
-                n = 0;
-            }
-        });
-    } else {
+const indeks = () => {
+  let welcomeUser = document.getElementById('welcomeUser');
+  const test = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:7777/");
+      const result = await response.json();
+      
+      welcomeUser.classList.toggle("d-none");
+      result.text.map(v => {
+        welcomeUser.innerHTML += v;
         setTimeout(() => {
-            confirm_keluar()
-        }, 500);
-        alert("Nama diperlukan!");
+          // 
+          }, 500);
+      });
+    } catch(err) {
+      welcomeUser.classList.toggle("d-none");
+      welcomeUser.innerHTML = err;
     }
+  }
+  
+  let n = 0;
+  let navbar_button = document.getElementsByClassName("navbar-toggler-icon");
+  let navbar_brand = document.getElementsByClassName("navbar-brand");
+  let profile = document.getElementsByClassName("profile");
+  
+  navbar_button[0].addEventListener('click', () => {
+    n+=1;
+    if ( n == 1 ){
+      navbar_brand[0].classList.remove("act")
+          navbar_brand[0].classList.toggle("none");
+          profile[0].style.display = "block";
+      } else if ( n == 2 ) {
+          navbar_brand[0].classList.remove("none");
+          navbar_brand[0].classList.toggle("act");
+          profile[0].style.display = "none";
+          n = 0;
+      }
+  });
 }
-home()
